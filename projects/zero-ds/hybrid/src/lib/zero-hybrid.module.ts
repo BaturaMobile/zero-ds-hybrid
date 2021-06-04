a0800345b6fd4b0826029b7151cb44c7707198a3
Aingeru <apalazuelo@baturamobile.com>
2021-06-04 21:48:44 +0200

    added ZeroHybridModule.forRoot()

--- projects/zero-ds/hybrid/src/lib/zero-hybrid.module.ts
+++ projects/zero-ds/hybrid/src/lib/zero-hybrid.module.ts
@@ -1,16 +1,36 @@
-import { NgModule } from '@angular/core';
+import { InjectionToken, ModuleWithProviders, NgModule } from '@angular/core';
 import { ZeroHybridComponent } from './zero-hybrid.component';
+import { ZeroHybridService } from './zero-hybrid.service';
+import { HttpClientModule } from '@angular/common/http';
 
+export interface LibConfig {
+  /** URL a la API del proyecto */
+  apirUrl?: string;
+}
 
-
+export const LibConfigService = new InjectionToken<LibConfig>('LibConfig');
 @NgModule({
   declarations: [
     ZeroHybridComponent,
   ],
   imports: [
+    HttpClientModule,
   ],
   exports: [
     ZeroHybridComponent,
   ]
 })
-export class ZeroHybridModule { }
+export class ZeroHybridModule {
+  static forRoot(config: LibConfig): ModuleWithProviders<LibConfig> {
+    return {
+      ngModule: ZeroHybridModule,
+      providers: [
+        ZeroHybridService,
+        {
+          provide: LibConfigService,
+          useValue: config
+        }
+      ]
+    }
+  }
+}
--- projects/zero-ds/hybrid/src/lib/zero-hybrid.service.ts
+++ projects/zero-ds/hybrid/src/lib/zero-hybrid.service.ts
@@ -1,9 +1,19 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { LibConfig, LibConfigService } from './zero-hybrid.module';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ZeroHybridService {
 
-  constructor() { }
+  private baseUrl = this.config.apirUrl;
+
+  constructor(
+    @Inject(LibConfigService) private config: LibConfig,
+    private http: HttpClient,
+  ) {
+    console.log('Config API URL: ', this.config);
+  }
+
 }
